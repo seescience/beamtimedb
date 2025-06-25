96b82fbc1ff066011b63bfa51e74c2123963ae3f
Matthew Newville <newville@cars.uchicago.edu>
2025-06-25 16:19:26 -0500

    add update_pvs

--- beamtimedb/__init__.py
+++ beamtimedb/__init__.py
@@ -1,5 +1,5 @@
 from .schema import create_beamtimedb
 from .beamtimedb import BeamtimeDB
-from .use_apsbss import filldb_from_apsbss
+from .use_apsbss import filldb_from_apsbss, update_pvs
 from .esafpdf import read_esaf_pdfs
 
